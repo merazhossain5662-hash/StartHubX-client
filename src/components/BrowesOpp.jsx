e8a76b197c5c1b5d17f54d64b0924dbb092b0305
boboiboy <merazhossain5662@gmail.com>
2026-08-01 15:45:45 +0600

    added total count on pagination

--- src/components/BrowesOpp.jsx
+++ src/components/BrowesOpp.jsx
@@ -100,7 +100,7 @@ const BrowesOpp = ({ oppData = [], searchQ }) => {
     selectedIndustries.length > 0,
   );
 
-  const totalItems = oppData.length;
+  const totalItems = oppData?.at(-1)?.totalCount || oppData.length;
   const totalPages = Math.max(1, Math.ceil(totalItems / itemsPerPage));
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = Math.min(startIndex + itemsPerPage, totalItems);
