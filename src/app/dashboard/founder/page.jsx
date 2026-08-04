1618f33b3babcbfaaf5c24cb7381a054567a0da1
boboiboy <merazhossain5662@gmail.com>
2026-08-04 20:35:39 +0600

    trying to fix founder overview error

--- src/app/dashboard/founder/page.jsx
+++ src/app/dashboard/founder/page.jsx
@@ -5,24 +5,19 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 
 const founderPage = async () => {
-  const { user, isPending } = await auth.api.getSession({
+  const session = await auth.api.getSession({
     headers: await headers(), // some endpoints might require headers
   });
+
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_URI}/api/startup/${user?.email}`,
+    `${process.env.NEXT_PUBLIC_URI}/api/startup/${session?.user?.email}`,
   );
   const startupData = await res.json();
-  const startupId = startupData?._id;
-  const ApplicationsRes = await fetch(
-    `${process.env.NEXT_PUBLIC_URI}/api/applications/${startupId}`,
-  );
-  const applicationsData = await ApplicationsRes.json();
-  const activeApplications =
-    applicationsData?.filter((app) => !app?.isOrphan) || [];
+  const startupId = startupData[0]?._id;
   const opportunityRes = await fetch(
     `${process.env.NEXT_PUBLIC_URI}/api/opportunities/${startupId}`,
   );
-  const opportunityData = await opportunityRes.json();
+  const { opportunities } = await opportunityRes.json();
   if (isPending) {
     return (
       <div className="flex items-center gap-4">
@@ -34,13 +29,13 @@ const founderPage = async () => {
   return (
     <div>
       <h1 className="text-lg md:text-2xl ">
-        Wellcome, <span className="font-bold ml-1">{user.name}</span>
+        Wellcome, <span className="font-bold ml-1">{session?.user?.name}</span>
       </h1>
       <p className="text-xs md:text-sm text-gray-500">
         Here's an overview of your startup activity.
       </p>
       <OverviewDiv
-        opportunityData={opportunityData || []}
+        opportunityData={opportunities || []}
         activeApplications={activeApplications}
       />
     </div>
