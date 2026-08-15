ecb308fb559a9297fa1c6190ea0aae2903092bb1
boboiboy <merazhossain5662@gmail.com>
2026-08-15 15:01:10 +0600

    console loging users in manage users page by better auth

--- src/app/dashboard/admin/manage-users/page.jsx
+++ src/app/dashboard/admin/manage-users/page.jsx
@@ -1,6 +1,26 @@
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 import React from "react";
 
-const manageUsers = () => {
+const manageUsers = async () => {
+  const users = await auth.api.listUsers({
+    query: {
+      // searchValue: "meraz",
+      // searchField: "name",
+      // searchOperator: "contains",
+      // limit: 100,
+      // offset: 100,
+      // sortBy: "name",
+      // sortDirection: "desc",
+      // filterField: "email",
+      // filterValue: "hello@example.com",
+      // filterOperator: "eq",
+    },
+
+    headers: await headers(),
+  });
+
+  console.log("users", users);
   return <div></div>;
 };
 
