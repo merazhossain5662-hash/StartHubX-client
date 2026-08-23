919df5a3ff4df0c491c922fe829ad2232dc212b3
boboiboy <merazhossain5662@gmail.com>
2026-08-23 06:31:07 +0600

    created transactions and manage-users folder

---
+++ src/app/dashboard/admin/manage-startups/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const manageStartups = () => {
+  return <div></div>;
+};
+
+export default manageStartups;
---
+++ src/app/dashboard/admin/transactions/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const transactionsPage = () => {
+  return <div></div>;
+};
+
+export default transactionsPage;
