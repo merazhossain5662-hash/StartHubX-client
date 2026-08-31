ff2ccde398810cefe877cac12de258c2992b2fc2
boboiboy <merazhossain5662@gmail.com>
2026-08-31 23:12:13 +0600

    protecting admin and profile routes fronend

--- src/app/profile/layout.jsx
+++ src/app/profile/layout.jsx
@@ -1,5 +1,6 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const profileLayout = async ({ children }) => {
