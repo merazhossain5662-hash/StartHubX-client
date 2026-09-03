0b99921559e635c2012ce85f7d42a3fb1692cb80
boboiboy <merazhossain5662@gmail.com>
2026-09-03 19:24:12 +0600

    fixing jwt from better auth on client side again

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import { admin } from "better-auth/plugins";
+import { admin, jwt } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.CONNECTION_STRING);
 const db = client.db("Start_Hub_X");
